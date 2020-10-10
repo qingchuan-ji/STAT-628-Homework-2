ea6d2a71caebce41fa100e718d64a3df3ed9d179
Qingchuan Ji <qji5@wisc.edu>
2020-10-10 11:12:40 +0800

    Using different linear regression method to fit the model

--- test.R
+++ test.R
@@ -11,7 +11,7 @@ head(BodyFat)
 #The unit of weight is lb and the unit of height is inches.
 
 #1. Multiple linear regression with all the independent variables
-lm1<-lm(BODYFAT~DENSITY+AGE+WEIGHT+HEIGHT+ADIPOSITY+NECK+CHEST+ABDOMEN+HIP+THIGH+KNEE+ANKLE+BICEPS+FOREARM+WRIST,data=BodyFat)
+lm1<-lm(BODYFAT~AGE+WEIGHT+HEIGHT+ADIPOSITY+NECK+CHEST+ABDOMEN+HIP+THIGH+KNEE+ANKLE+BICEPS+FOREARM+WRIST,data=BodyFat)
 summary(lm1)
 
 #2. Using stepwise regression
@@ -19,16 +19,16 @@ lm2<-step(lm1)
 summary(lm2)
 
 #3. Only selecting the obvious variables in the lm2 
-lm3<-lm(BODYFAT~DENSITY+AGE+WEIGHT,data=BodyFat)
+lm3<-lm(BODYFAT~AGE+WEIGHT+NECK+ABDOMEN+THIGH+FOREARM+WRIST,data=BodyFat)
 summary(lm3)
 plot(lm3)
 #4. LASSO Regression
-x<-as.matrix(BodyFat[,3:17])
+x<-as.matrix(BodyFat[,4:17])
 y<-as.matrix(BodyFat[,2])
 lm4<-lars(x,y,type = "lasso")
 lm4
 plot(lm4)
-summary(lm4)#Select step=4, Cp=0.2283
+summary(lm4)#Select step=11, Cp=9.5408
 lm4$beta
-coef <-coef.lars(lm4,mode="step",s=5)
+coef <-coef.lars(lm4,mode="step",s=12)
 coef
